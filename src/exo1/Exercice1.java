a22476fd49efea648f90d452d23f1711db46b43f
remy_tabardel <rtabardel@oui.sncf>
2020-11-03 14:10:54 +0100

    exercice 1

---
+++ src/exo1/Exercice1.java
@@ -0,0 +1,16 @@
+package exo1;
+
+/*
+0 - ne pas toucher au constructeur de Exercice1
+1 - créer dans Exercice1 une variable locale privée nom
+2 - créer dans Exercice1 une variable statique prenom
+3 - créer dans Exercice1 une méthode locale changerNom qui permet d'assigner une nouvelle valeur au nom
+4 - créer dans Exercice1 une méthode statique afficherNomPrenom qui permet d'afficher les infos dans la console (nom - prenom)
+5 - appeler afficherNomPrenom depuis main et faire en sorte que Solène Krognole s'affiche
+
+ */
+public class Exercice1 {
+    static void start(){
+
+    }
+}
