08745b6a8daf718275383013ba835bb1543b3ffe
remy_tabardel <rtabardel@oui.sncf>
2020-11-03 14:38:59 +0100

    exercice 1

---
+++ src/exo2/Exercice2.java
@@ -0,0 +1,22 @@
+package exo2;
+
+/*
+1- Ne rien toucher dans le fichier Exerice2.java
+2- Depuis main, afficher à la suite ces deux phrases avec la classe Exercice2
+"Coucou je suis Solène Krognole, j'ai 31 ans et j'habite en France"
+"Coucou je suis Rémy Choup, j'ai 30 ans et j'habite en Choupicardie"
+ */
+public class Exercice2 {
+    private final String nom;
+    private String prenom;
+    String age;
+    static String pays;
+
+    Exercice2(String nom){
+        this.nom = nom;
+    }
+
+    void afficher(){
+        System.out.println("Coucou, je suis "+nom+" "+prenom+", j'ai "+age+" ans et j'habite en "+pays);
+    }
+}
