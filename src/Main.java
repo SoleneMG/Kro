169b52630980b7a2ad8c78de0be2a7e2c415e577
remy <remytabardel@live.fr>
2020-11-03 13:45:04 +0100

    HelloWorld

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import hello.HelloWorld;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        HelloWorld HelloWorld = new HelloWorld("Rémy");
+        HelloWorld.printHelloWorld();
+    }
+}
---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class HelloWorld {
+String name;
+    public HelloWorld(String txt) {
+        this.name = txt;
+    }
+    public void printHelloWorld(){
+        System.out.println("Hello World by " +name);
+    }
+
+    }
