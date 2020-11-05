32792707220053ca6c610eded699eb429680cf91
remy_tabardel <rtabardel@oui.sncf>
2020-11-05 09:38:28 +0100

    exercise 6 & 7 & 8

--- src/exo6/Exercise6.java
+++ src/exo6/Exercise6.java
@@ -2,11 +2,10 @@ package exo6;
 
 /**
  * Créer une classe Person avec les variables suivantes, voir si elles sont statiques ou pas:
- * (on considère qu'il n'y a pas d'handicapé)
  * name
- * numberOfEyes
+ * numberMaxOfEyes
  * eyesColor
- * numberOfFingers
+ * numberMaxOfFingers
  * size
  * firstname
  * language
