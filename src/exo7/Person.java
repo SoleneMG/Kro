6a279ad71223e653eba17328eca80444012cbb93
remy <remytabardel@live.fr>
2020-11-05 15:46:07 +0100

    Exercice 7 corrigé

--- src/exo7/Person.java
+++ src/exo7/Person.java
@@ -2,17 +2,22 @@ package exo7;
 
 public class Person {
     private final String name;
+    String nameOfcat;
+    String nameOfdog;
 
 
     public Person(String name, Dog dog, Cat cat) {
         this.name = name;
+        nameOfdog = dog.getName();
+        nameOfcat = cat.getName();
     }
     public Person(String name, Dog dog) {
         this.name = name;
+        nameOfdog = dog.getName();
     }
     public Person(String name,Cat cat) {
         this.name = name;
-
+        nameOfcat = cat.getName();
     }
     public Person(String name) {
         this.name = name;
@@ -20,7 +25,13 @@ public class Person {
 
     public void describeYourSelf() {
 
-        System.out.print("Je m'appelle " + name + ".");
+        System.out.print("Je m'appelle " + name + "."+"\n");
+        if (nameOfdog != null){
+            System.out.print("Mon chien s'appelle "+nameOfdog+".");
+        }
+        if (nameOfcat != null){
+            System.out.print("Mon chat s'appelle "+nameOfcat+".");
+        }
 
 
     }
