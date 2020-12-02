6d9d78a48c68cab47e7821bb12ef434cd0e76fbf
remy_tabardel <rtabardel@oui.sncf>
2020-12-02 14:44:25 +0100

    exercise 18

--- src/exo18/Exercice18.java
+++ src/exo18/Exercice18.java
@@ -8,7 +8,7 @@ public class Exercice18 implements Runnable{
         FolderGenerator folderGenerator = new FolderGenerator();
         Folder parentFolder = folderGenerator.generate(5);
 
-        System.out.println("Tu dois avoir "+folderGenerator.getLastGenerationFoldersCount()+" dossiers (incluant le parent) et "+folderGenerator.getLastGenerationFilesCount()+" fichiers, sinon tu peux krogonoler");
+        System.out.println("Tu dois avoir "+folderGenerator.getLastGenerationFoldersCount()+" dossiers (incluant le parent) et "+folderGenerator.getLastGenerationFilesCount()+" fichiers, sinon tu peux krognoler");
 
         //calculer le nombre de dossiers du parentFolder (incluant le parent) avec une méthode récursive
     }
